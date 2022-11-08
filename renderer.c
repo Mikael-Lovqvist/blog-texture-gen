a237cb0ddb74c9f2872728a4fd67d5083d7aceaa
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2022-11-08 06:50:57 +0100

    Archived the voronoi renderer, added normalized cumulative distance rendering

--- renderer.c
+++ renderer.c
@@ -12,7 +12,7 @@ typedef struct rendering_settings {
 } rendering_settings;
 
 
-void render_line(pixel_format frame[HEIGHT][WIDTH], int y, rendering_settings* rs) {
+void voronoi_render_line(pixel_format frame[HEIGHT][WIDTH], int y, rendering_settings* rs) {
 
 	pixel_format* row = frame[y];
 	for (int x=0; x<WIDTH; x++) {
@@ -61,6 +61,64 @@ void render_line(pixel_format frame[HEIGHT][WIDTH], int y, rendering_settings* r
 		}
 
 		row[x] = frgb(1-closest_distance*50, 1-closest_distance*10, 1-closest_distance*5);
+
+	}
+
+
+}
+
+
+
+
+
+
+void render_line(pixel_format frame[HEIGHT][WIDTH], int y, rendering_settings* rs) {
+
+	pixel_format* row = frame[y];
+	for (int x=0; x<WIDTH; x++) {
+
+		vec2 check_points[4];
+		vec2 main_point = v2(
+			(float)x / (float)(WIDTH-1),
+			(float)y / (float)(HEIGHT-1)
+		);
+
+		vec2 cp_offset;
+
+		//Check point 1
+		if ((main_point.x < 0.5) && (main_point.y < 0.5)) {	//Top left quadrant
+			cp_offset = v2(1, 1);
+		} else if ((main_point.x > 0.5) && (main_point.y < 0.5)) {	//Top right quadrant
+			cp_offset = v2(-1, 1);
+		} else if ((main_point.x < 0.5) && (main_point.y > 0.5)) {	//Bottom left quadrant
+			cp_offset = v2(1, -1);
+		} else {	//Bottom right quadrant
+			cp_offset = v2(-1, -1);
+		}
+
+		check_points[0] = main_point;
+		check_points[1] = v2(main_point.x + cp_offset.x, main_point.y);
+		check_points[2] = v2(main_point.x, main_point.y + cp_offset.y);
+		check_points[3] = v2(main_point.x + cp_offset.x, main_point.y + cp_offset.y);
+
+
+		double cumulative_weight = 0;
+
+		for (int i=0; i<rs->num_points; i++) {
+
+			float this_distance = magn_v2(sub_v2_v2(check_points[0], rs->points[i].position));
+			for (int cp=1; cp<4; cp++) {
+				float distance_candidate = magn_v2(sub_v2_v2(check_points[cp], rs->points[i].position));
+				this_distance = min(this_distance, distance_candidate);
+			}
+
+			cumulative_weight += pow(1.0 - ((double)this_distance / sqrt(2)), 25) / (double) (WIDTH * HEIGHT) ;
+
+		}
+
+		double gain=100000;
+
+		row[x] = frgb(cumulative_weight*50*gain, cumulative_weight*10*gain, cumulative_weight*5*gain);
 
 	}
 
